d9e8a2b4c9a184f7f6efa54d38a067f025f2fe03
erik <erikhervall11@gmail.com>
2024-06-14 09:54:58 -0700

    email fixed

--- assets/js/email.js
+++ assets/js/email.js
@@ -5,24 +5,31 @@
 */
 
 (function ($) {
+  emailjs.init("-Mx4RZlW2OaO_e7tv");
+
   document
     .getElementById("email-form")
     .addEventListener("submit", function (e) {
+      e.preventDefault();
       const name = document.getElementById("name").value;
       const email = document.getElementById("email").value;
       const message = document.getElementById("message").value;
 
       emailjs
         .send("service_ddqquj7", "template_m9cdb1k", {
-          name,
-          email,
-          message,
+          name: name,
+          email: email,
+          message: message,
         })
         .then(
-          function (res) {
+          function (response) {
             document.getElementById("res").innerText =
               "Thanks For Reaching Out!";
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
           },
+
           function (error) {
             document.getElementById("res").innerText = JSON.stringify(error);
           }
